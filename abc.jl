dca865eac61560bc8d6ba0af033df5517989538f
likewu <likewu@126.com>
2021-11-18 10:48:43 +0800

    ee

---
+++ abc.jl
@@ -0,0 +1,14 @@
+abcdefghijklmnopqrstuvwxyz
+
+abcdefghijklmnopqrstuvwxyz
+
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+
+
+help date time exit true false if for on off red green blue
+
+
+x=[5,1,8,2,12,9,3,7]
+
+sort(x)
---
+++ plot.jl
@@ -0,0 +1,11 @@
+using UnicodePlots
+
+print("hhhhhhhhhhhhhhhhhhhh\n")
+ans = barplot([1,2,3,4,5,6,7], [7,14,35,21,35,28,28])
+
+
+#ans = lineplot([-1, 2, 3, 7], [-1, 2, 9, 4])
+#ans = scatterplot([-1,1,2,3,4,5,6,7], [-1,1,2,5,3,5,4,4])
+
+
+print(ans)
