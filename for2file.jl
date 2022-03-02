9dcea67ffd7125968d057bdb74152bea062210c1
likewu <likewu@126.com>
2022-03-02 08:40:10 +0800

    aa

--- for2file.jl
+++ for2file.jl
@@ -1,13 +1,16 @@
-open("for2file2.txt", "w") do io
+using Printf
+
+open("for2file3.txt", "w") do io
   sleep(5)
 
 
 
-  for i = 1:150000000000000000000000000000000
+  for i = 0:150000000000000000000000000000000
     sleep(0.1)
 
     #write(io, string(i," "))
-    write(io, string(2^i," "))
+    write(io, @sprintf("%16s\n",string(i,base=2)))
+    #write(io, string(2^i," "))
 
     flush(io)
 
