6ba02e4d0194404ccf667e3544f31b6b589aef9a
likewu <likewu@126.com>
2022-05-23 17:52:28 +0800

    aa

---
+++ if.jl
@@ -0,0 +1,24 @@
+a1="hhhhhhhhhhhhhhhggggggggg"
+a2=[1, 3, 5, 7, 9]
+a3=(100, 45)
+
+
+tanke=100
+tanke=tanke+50
+tanke=tanke+66
+
+
+
+aa=2
+
+if aa==1
+
+  print123("1111111111111111",:blue)
+  #print4(:dog, :green)
+  #print123(tanke)
+elseif anniu==2
+
+  print123("22222222222222222222",:red)
+  #print4(:shark, :blue)
+
+end
---
+++ if2.jl
@@ -0,0 +1,15 @@
+x=4
+if x==4
+
+print123("1111111111666666",:red)
+
+
+
+print123("")
+
+ 
+
+print123("木头fvvgfgfxffygcggv",:green)
+
+
+end
