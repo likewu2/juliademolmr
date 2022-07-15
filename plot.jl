695f1d3de31a98030b46780ec64b4a0bfe77cb1e
likewu <likewu@126.com>
2022-07-15 16:01:16 +0800

    arrow

--- plot.jl
+++ plot.jl
@@ -8,7 +8,7 @@ println("hhhhhhhhhhhhhhhhhhhh")
 #ans = barplot([1,2,3,4,5,6,7], [7,14,35,21,35,28,28])
 
 
-ans = plot([-1, 2, 3, 7], [-1, 2, 9, 4])
+ans = plot([-1, 2, 3, 7], [-1, 2, 9, 4], xlims=(-2,9), ylims=(-2,9), arrow=true)
 #ans = scatterplot([-1,1,2,3,4,5,6,7], [-1,1,2,5,3,5,4,4])
 
 #display(ans)
