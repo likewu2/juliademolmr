0ba5d6c1c03001b9d034f1308056cf4423064cc4
likewu <likewu@126.com>
2022-07-15 15:46:30 +0800

    gui

--- plot.jl
+++ plot.jl
@@ -3,7 +3,7 @@ using Plots;
 #unicodeplots()
 #gr()
 
-#println("hhhhhhhhhhhhhhhhhhhh")
+println("hhhhhhhhhhhhhhhhhhhh")
 
 #ans = barplot([1,2,3,4,5,6,7], [7,14,35,21,35,28,28])
 
@@ -11,8 +11,10 @@ using Plots;
 ans = plot([-1, 2, 3, 7], [-1, 2, 9, 4])
 #ans = scatterplot([-1,1,2,3,4,5,6,7], [-1,1,2,5,3,5,4,4])
 
-
-display(ans)
+#display(ans)
 gui()
 
 #print(ans)
+
+
+println("hhhhhhhhhhhhhhhhhhhh")
