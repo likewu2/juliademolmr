855e8c9b6a9ac3fb8ebc8e6a3b0d39b3b0545ac5
likewu <likewu@126.com>
2022-07-15 16:30:59 +0800

    init

--- plot.jl
+++ plot.jl
@@ -1,20 +1,21 @@
 #using UnicodePlots
-using Plots; 
+using Plots
 #unicodeplots()
 #gr()
 
-println("hhhhhhhhhhhhhhhhhhhh")
-
 #ans = barplot([1,2,3,4,5,6,7], [7,14,35,21,35,28,28])
 
+x=[0, 1, 2, 3, 4, 5]
+y=[0, 1, 2, 3, 4, 5]
 
-ans = plot([-1, 2, 3, 7], [-1, 2, 9, 4], xlims=(-2,9), ylims=(-2,9), arrow=true)
+ans = plot(x,y)
 #ans = scatterplot([-1,1,2,3,4,5,6,7], [-1,1,2,5,3,5,4,4])
+
+xlims!((-2,10))
+ylims!((-2,10))
+arrow(-2,0,12,0)
 
 #display(ans)
 gui()
 
 #print(ans)
-
-
-println("hhhhhhhhhhhhhhhhhhhh")
