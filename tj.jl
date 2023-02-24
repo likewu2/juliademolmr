c1f1c47614779c2b2a687f86ab0c0dc4caf39f48
likewu <likewu@126.com>
2023-02-24 15:20:25 +0800

    jj

--- tj.jl
+++ tj.jl
@@ -1,10 +1,11 @@
-x=1
-if x==8
+miebashoutao=2000+1000 +1000+1000+1000+4000
 
-  print("花花")
+if miebashtao>10000 && miebashtao<20000
+
+  print("升级到2级")
 
 else
 
-  print("朵朵")
-
+  print("1级")
+  
 end
