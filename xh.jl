a1503f3578137508f4861e493401d12661eb4295
likewu <likewu@126.com>
2023-08-11 12:42:47 +0800

    aa

--- xh.jl
+++ xh.jl
@@ -1,16 +1,35 @@
 a=[1,2,3]
 b=[6,7,8]
-cat(a,b)
+#cat(a,b)
 sort(a)
 sum(a)
 sum(b)
-plot(a,b)
+#plot(a,b)
 
+print123("第一个循环:")
 for x in [1,3,66,86,5,5]
 
-  push(a,x)
+  #push(a,x)
 
   print123(x)
 end
 
 print123(a)
+
+
+
+print123("第二个循环:")
+w=0
+for x in [1,2,3,4,5]
+  global
+  w=w+x
+end
+print123(w)
+
+print123("第三个循环:")
+w=0
+for x in  1:10000000000
+  global
+  w=w+x
+end
+print123(w)
