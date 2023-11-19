d1b244ce5f40fe773f00db75e172d995f12390a2
likewu <likewu@126.com>
2023-11-19 20:45:25 +0800

    jifen

--- tj.jl
+++ tj.jl
@@ -5,10 +5,20 @@ miebashoutao=2000+1000 +1000+1000+1000+4000
 
 if miebashoutao>10000 && miebashoutao<20000
 
-  print("升级到2级")
+  #print("升级到2级")
 
 else
 
-  print("1级")
+  #println("1级")
   
 end
+
+
+jifen=5000
+dengji=0
+if jifen>=5000 && jifen<10000
+  dengji=1
+end
+
+print("等级：")
+println(dengji)
