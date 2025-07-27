64f2a01e341ecc503a05fbcd8409f0fed9c188c5
likewu <likewu@126.com>
2025-07-27 16:26:09 +0800

    aa

--- jitu.jl
+++ jitu.jl
@@ -1,11 +1,17 @@
-# 笼子里有5个头和16只脚
+# 笼子里有50个头和160只脚
 head=5
 leg=16
+aa=0.01
+bb="aaaaabbbbbbbb"
+cc=true
+dd=false
 
 for ji in 0:head
   
   tuzi=head-ji
   jiao=ji*2+tuzi*4
-  println("鸡：",ji,"  兔子：",tuzi,"  脚：",jiao)
+  if jiao==leg
+    println("鸡：",ji,"  兔子：",tuzi,"  脚：",jiao)
+  end
 
 end
