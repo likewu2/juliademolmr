22176f4c8c24449480a084feedee783bc7b68368
likewu <likewu@126.com>
2025-10-04 23:23:05 +0800

    aa

--- move_box.jl
+++ move_box.jl
@@ -1,24 +1,26 @@
+#using AlgebraOfGraphics
+#using CairoMakie
+#using WGLMakie
 using GLMakie
 
-box = Rect2f(9, 1, 2, 2)
+box = Rect2f(3, 1, 2, 2)
 
-f = Figure(size = (500, 500))
+f = Figure(size = (700, 700))
 a = Axis(f[1, 1], aspect = DataAspect())
-xlims!(a, 0, 12); a.xticks = 1:1:11
-ylims!(a, 0, 12); a.yticks = 1:1:11
+xlims!(a, -11, 11); a.xticks = -10:1:10
+ylims!(a, -11, 11); a.yticks = -10:1:10
 
 # Initial plot for reference
 scatterlines!(a, box, color = 1:4, markersize = 20, linewidth = 5)
 
 # Transformed plot
 p2 = scatterlines!(a, box, color = 1:4, markersize = 20, linewidth = 5)
+#origin!(p2, 3,0,0) # apply rotation & scaling relative to the center of the box
+center_point = Point2f(3.0, 0.0)  # The point you wanted as origin
+translate!(p2, -center_point...)  # Move to origin
 
-#origin!(p2, 1,0,0) # apply rotation & scaling relative to the center of the box
-center_point = Point2f(1.0, 0.0)
-#translate!(p2, -center_point...)  # Move to origin
-
-scale!(p2, 2, 2)       # double x, y
-rotate!(p2, pi/2)      # 90° rotation
-translate!(p2, -5, 5)
+scale!(p2, 2.0, 2.0)       # double x, y
+rotate!(p2, deg2rad(90))      # 90° rotation
+translate!(p2, -2, 1)
 
 f
