28097dab545088eddd1482580da0d1e4cc191772
likewu <likewu@126.com>
2025-10-05 16:32:06 +0800

    aa

--- move_box.jl
+++ move_box.jl
@@ -3,7 +3,7 @@
 #using WGLMakie
 using GLMakie
 
-box = Rect2f(3, 1, 2, 2)
+box = Rect2f(-1, -1, 2, 2)
 
 f = Figure(size = (700, 700))
 a = Axis(f[1, 1], aspect = DataAspect())
@@ -16,11 +16,11 @@ scatterlines!(a, box, color = 1:4, markersize = 20, linewidth = 5)
 # Transformed plot
 p2 = scatterlines!(a, box, color = 1:4, markersize = 20, linewidth = 5)
 #origin!(p2, 3,0,0) # apply rotation & scaling relative to the center of the box
-center_point = Point2f(3.0, 0.0)  # The point you wanted as origin
-translate!(p2, -center_point...)  # Move to origin
+#center_point = Point2f(4.0, 2.0)  # The point you wanted as origin
+#translate!(p2, -center_point...)  # Move to origin
 
 scale!(p2, 2.0, 2.0)       # double x, y
 rotate!(p2, deg2rad(90))      # 90° rotation
-translate!(p2, -2, 1)
+translate!(p2, -5, 5)
 
 f
